38ea23b0012f7d2c71d10743e3aba8ad8460d288
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-03 23:51:54 +0500

    fix

--- backend/get_links.php
+++ backend/get_links.php
@@ -3,7 +3,7 @@ require_once 'config.php';
 
 $user_id = $_POST['user_id'];
 
-$result = $mysql->query("SELECT * FROM links WHERE user_id = $user_id");
+$result = $mysql->query("SELECT * FROM links WHERE");
 
 echo json_encode($result->fetch_assoc());
 ?>
