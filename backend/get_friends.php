1408ae5e5b475ad7fcb8e30b0cf109b5fcfedd46
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-04 00:20:55 +0500

    fix

--- backend/get_friends.php
+++ backend/get_friends.php
@@ -5,13 +5,19 @@ $user_id = $_POST["user_id"];
 
 $result = $mysql->query("SELECT * FROM users WHERE referrer_id = '$user_id'");
 $friends = array();
-while($row = $result->fetch_assoc()) {
+while ($row = $result->fetch_assoc()) {
     $friends[] = array(
         'user_id' => $row['user_id'],
-        'username' => $row['username'],
+        'username' => $row['username'], 
         'level' => $row['level'],
-        'join_date' => $row['join_date']
+        'balance' => $row['balance'],
+        'invited_users' => $row['invited_users'],
+        'join_date' => $row['join_date'],
+        'zadanie_1' => $row['zadanie_1'],
+        'zadanie_2' => $row['zadanie_2'], 
+        'zadanie_3' => $row['zadanie_3'],
+        'zadanie_4' => $row['zadanie_4'],
+        'zadanie_5' => $row['zadanie_5']
     );
 }
 echo json_encode($friends);
-?>
