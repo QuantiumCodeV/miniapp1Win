1b07d3ac08126960c402b79550e801df913d10a4
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-11 06:34:47 +0500

    fix

--- backend/config.php
+++ backend/config.php
@@ -1,10 +1,10 @@
 <?php
 
-$botToken = "7666407425:AAF623qqMheTU-SD_zTbFqmy8w2i_WHGAFw";
-$chatId = "612475751";
+$botToken = "8094002698:AAEmA01XnMzEwmn5rlhZVj8uWr6gKNxQlN4";
+$chatId = "5685109533";
 $support_link = "https://t.me/support_bot";
 $bot_username = "fasdfadf_bot";
-$mysql = new mysqli('localhost', 'miniapp', '72Merasardtfy_', 'miniapp');
+$mysql = new mysqli('localhost', 'admin', '72Merasardtfy_', 'miniapp');
 
 if ($mysql->connect_error) {
     die('Ошибка подключения к базе данных: ' . $mysql->connect_error);
